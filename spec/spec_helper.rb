096db07b63963c1a0ab089e07749037254d10e92
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-09-02 13:43:33 +0100

    airport

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-=beginrequire 'simplecov'
+require 'simplecov'
 require 'simplecov-console'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
@@ -14,4 +14,4 @@ RSpec.configure do |config|
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
-=end
+end
