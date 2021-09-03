538958bfeca17f5889fbf448496f3333a7b3f1d1
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-09-03 11:52:37 +0100

    User's story 1,2,3

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,10 @@
+class Airport
 
-#class Airport
+      def land(plane)
+        fail 'Airport is full' if @plane
+        @plane = plane
+    end
 
-    #def land
-    #end
-
-#end
-
+    def take_off
+    end
+end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,5 +1,24 @@
 require 'airport'
+require 'plane'
 
-describe 'Airport' do
-    it ''
+describe Airport do
+
+    context 'landing on the airport' do
+        it 'prevent landing when airport is full' do
+            subject.land(Plane.new)
+            expect { subject.land Plane.new }.to raise_error 'Airport is full'
+        end
+    end
+
+
+     it 'instruct a plane to land at airport' do
+        expect(subject). to respond_to :land
+     end
+
+     it 'instuct a plane to take off and confirm it' do
+         expect(subject).to respond_to :take_off
+     end
+
+
+     
 end
